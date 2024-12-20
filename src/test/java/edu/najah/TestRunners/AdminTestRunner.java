10c177890efbaf26c4ff427f5ba67ef9bc817f77
Arqam Mousa <arqam.mousa@gmail.com>
2024-12-20 14:45:54 +0200

    Added Test runners for each group

--- src/test/java/edu/najah/TestRunners/AdminTestRunner.java
+++ src/test/java/edu/najah/TestRunners/AdminTestRunner.java
@@ -1,4 +1,4 @@
-package edu.najah;
+package edu.najah.TestRunners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -6,12 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features", // Path to your feature files
-        glue = "stepdefinitions", // Package containing step definitions
+        features = "src/test/resources/features/AdminFeatures", // Path to your feature files
+        glue = "stepdefinitions/AdminStepDefinitions", // Package containing step definitions
         plugin = {"pretty", "html:target/cucumber-reports.html"}, // Report generation
         monochrome = true // Makes output readable
 )
 
-public class TestRunner {
+public class AdminTestRunner {
 
 }
